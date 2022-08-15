c8a8d06444627e671268d21a17800ab5afa03e22
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-08-15 22:14:17 +0300

    corrected some errors in review strings lab

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const currentUser = 'Alice Kemunto';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ALICE KEMUNTO!`;
-const shortGreeting = 'Welcome, A!'
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
