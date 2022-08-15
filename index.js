1e4f3d3b6086a46b9058a24706797abaa72783e3
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-08-15 21:57:30 +0300

    completed review strings lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Alice Kemunto';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ALICE KEMUNTO!`;
+const shortGreeting = 'Welcome, A!'
